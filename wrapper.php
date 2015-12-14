6855fcb9867f959b6826fe587ea59b7be4ef4e08
Rick <rickzhang@live.ca>
2015-12-13 22:37:09 -0500

    I MIGHT BE A GENIUS

--- wrapper.php
+++ wrapper.php
@@ -13,7 +13,7 @@
 			{
 				$head[] = $v;
 				if( preg_match( "#HTTP/[0-9\.]+\s+([0-9]+)#",$v, $out ) )
-					$head['response_code'] = intval($out[1]);
+					http_response_code(intval($out[1]));
 			}
 		}
 		return $head;
@@ -23,10 +23,6 @@
   header('Content-Type: application/json');
   $url = $_POST['url'];
   $json = file_get_contents($url.'api_key=325f0342-8487-463f-be24-96ed6fb11bf1');
-  if($json.empty()) {
-    echo json_encode($http_response_header);
-  } else {
-	$json['response'] = 200;
-	echo $json;
-  }
+  parseHeaders($http_response_header);
+  echo $json;
 ?>
