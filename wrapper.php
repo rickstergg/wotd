6a902f4bd6e8bab81cfb5f14d0ef7863d9fdd560
Rick <rickzhang@live.ca>
2015-12-13 21:54:48 -0500

    Aight lets try this.

--- wrapper.php
+++ wrapper.php
@@ -13,7 +13,7 @@
 			{
 				$head[] = $v;
 				if( preg_match( "#HTTP/[0-9\.]+\s+([0-9]+)#",$v, $out ) )
-					$head['reponse_code'] = intval($out[1]);
+					$head['response_code'] = intval($out[1]);
 			}
 		}
 		return $head;
@@ -23,6 +23,6 @@
   header('Content-Type: application/json');
   $url = $_POST['url'];
   $json = file_get_contents($url.'api_key=325f0342-8487-463f-be24-96ed6fb11bf1');
+  $json['res_code'] = parseHeaders($http_response_header)['response_code'];
   echo $json;
-  echo parseHeaders($http_response_header));
 ?>
