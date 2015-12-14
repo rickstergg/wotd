7829c578b2a49f77c749226fea8eca5ab11fe356
Rick <rickzhang@live.ca>
2015-12-13 22:24:04 -0500

    Alright, SLEEP IS SOON BOYZ

--- wrapper.php
+++ wrapper.php
@@ -23,6 +23,10 @@
   header('Content-Type: application/json');
   $url = $_POST['url'];
   $json = file_get_contents($url.'api_key=325f0342-8487-463f-be24-96ed6fb11bf1');
-  echo $json;
-  echo json_encode($http_response_header);
+  if($json.empty()) {
+    echo json_encode($http_response_header);
+  } else {
+	$json['response'] = 200;
+	echo $json;
+  }
 ?>
